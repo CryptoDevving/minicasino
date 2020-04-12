2a567a7a2acc14a9b3e39ed93747004e2829209b
Gregers Rørdam <gregers@rordam.dk>
2020-04-12 16:34:05 +0200

    Wheel is now composed of polygons, and the numbers are rotated correctly

--- src/WheelApplet/presentational components/Wheel.js
+++ src/WheelApplet/presentational components/Wheel.js
@@ -23,25 +23,30 @@ export default function Wheel(props){
     let centery = height / 2;
     let lineColor="black";
 
-    let lines=[];
+    let triangles=[];
     let i;
     for (i=0;i < fields;i++)
     {
         let angle = i * 360/fields;
+        let angle2 = (i + 1) * 360/fields;
         let x1 = Math.cos(toRadians(angle)) * r + centerx;
         let y1 = Math.sin(toRadians(angle)) * r + centery;
-        lines.push(
+        let x2 = Math.cos(toRadians(angle2)) * r + centerx;
+        let y2 = Math.sin(toRadians(angle2)) * r + centery;
+        triangles.push(
             <React.Fragment key={i}>
-                <line
-                    x1={x1}
-                    y1={y1}
-                    x2={centerx}
-                    y2={centery}
-                    strokeWidth={1}
-                    stroke={lineColor}
+                <polygon
+                    points={x1 + "," + y1 + " " + centerx + "," + centery + " " + x2 + "," + y2}
+                    style={{
+                        fill: "lime",
+                        stroke: lineColor,
+                        strokeWidth: 1
+                    }}
                 />
-                <text x={Math.cos(toRadians(angle + 360 / fields / 2)) * r * 5/6 + centerx - 5}
-                      y={Math.sin(toRadians(angle + 360 / fields / 2)) * r * 5/6 + centery + 5}>
+                <text x={Math.cos(toRadians(0)) * r * 5/6 + centerx}
+                      y={Math.sin(toRadians(0)) * r * 5/6 + centery}
+                      transform={`rotate(${(angle + angle2)/2} ${centerx} ${centery})`}
+                >
                     {i+1}
                 </text>
             </React.Fragment>
@@ -50,8 +55,7 @@ export default function Wheel(props){
 
     return (
         <svg height={height} width={width} className={classes.animatedItem} {...rest}>
-            <circle cx={width/2} cy={height/2} r={r} stroke={lineColor} strokeWidth={1} fill="none"/>
-            {lines}
+            {triangles}
             Sorry, your browser does not support inline SVG.
 
         </svg>
